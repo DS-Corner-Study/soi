c6639ecaa09bf95830c7122d4a74db1cb26b46c8
soyyyyy <da5868@naver.com>
2023-10-30 05:35:14 +0900

    [1030] 5주차 스터디

---
+++ week5/ch5/src/component/Body.js
@@ -0,0 +1,32 @@
+// import React from "react";
+import "./Body.css";
+
+function Body({children}) {
+    console.log(children);
+    return (
+        <div className="body">
+            {children} 
+        </div>
+    );
+}
+
+export default Body;
+
+
+/*
+function Body({name, location, favorList}) {
+    console.log(name, location, favorList);
+    return (
+        <div className="body">
+            {name}은 {location}에 거주합니다.
+            <br />
+            {favorList.length}개의 음식을 좋아합니다. 
+        </div>
+    );
+}
+
+Body.defaultProps = {
+    favorList: [],
+};
+
+*/
