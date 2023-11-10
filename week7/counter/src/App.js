1dbc946b9f6fcb46c8573c748809d112147495e4
soyyyyy <da5868@naver.com>
2023-11-11 02:52:18 +0900

    [1111] 7주차 스터디 - ch6

--- week7/counter/src/App.js
+++ week7/counter/src/App.js
@@ -1,22 +1,60 @@
-
 import './App.css';
 import Viewer from './components/Viewer';
 import Controller from './components/Controller';
-import { useState } from 'react';
+import Even from './components/Even';
+import { useEffect, useRef, useState } from 'react';
 
 function App() {
   const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+
+  // const didMountRef = useRef(false);
+
   const handleSetCount = (value) => {
     setCount(count + value);
   };
 
+  const hadleChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  /*
+  useEffect(() => {
+    if (!didMountRef.current){
+      didMountRef.current = true;
+      return;
+    }
+    else {
+      console.log("컴포넌트 업데이트");
+    }
+  });
+
+  useEffect(() => {
+    console.log("컴포넌트 마운트");
+  }, []);
+
+  useEffect(() => { 
+    const intervalID = setInterval(() => {
+      console.log("깜빡");
+    }, 1000);
+
+    return () => {  // 클린업 - 콜백함수 실행 전 or 컴포넌트 언마운트 시점에 실행
+      console.log("클린업");
+      clearInterval(intervalID);
+    }
+  });
+  */
 
   return (
     <div>
       <div className='App'>
         <h1>Simple Counter</h1>
         <section>
+          <input value={text} onChange={hadleChangeText} />
+        </section>
+        <section>
           <Viewer count={count} /> 
+          { count % 2 === 0 && <Even /> }
         </section>
         <section>
           <Controller handleSetCount={handleSetCount} />
