af6ed7d8821b912b0be8f33b6b817fd2d210f79b
soyyyyy <da5868@naver.com>
2023-11-20 02:48:41 +0900

    [1120] 8주차 스터디

---
+++ week8/e-book/hooks-tutorial/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// import Info from './Info';
+import Average from './Average';
+
+const App = () => {
+  // return <Info />;
+  return <Average />;
+};
+ 
+export default App;
+
+
